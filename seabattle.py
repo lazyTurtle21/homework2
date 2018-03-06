0fa9f0207400158636a5482cd66018a2bd078029
lazyTurtle21 <31816443+lazyTurtle21@users.noreply.github.com>
2018-03-06 20:39:26 +0200

    added a possibility to choose if you want to see field with or without ships

--- seabattle.py
+++ seabattle.py
@@ -4,18 +4,50 @@ from functions import *
 
 
 class Game:
+    """Class for Game representation"""
     def __init__(self, fields, players, current_player):
+        """
+        Start new Game
+        :param fields: list
+        :param players: list
+        :param current_player: int
+
+        >>> game = Game([[1], [2]], ['Dobosevych', 'Romanyuk'], 1)
+        >>> game._Game__fields
+        [[1], [2]]
+        >>> game._Game__players
+        ['Dobosevych', 'Romanyuk']
+        >>> game._Game__current_player
+        1
+        """
         self.__fields = fields[:]
         self.__players = players[:]
         self.__current_player = current_player
 
     def field_with_ships(self, index):
+        """
+        Return field with ships as string
+        :param index: int
+        :return: str
+        """
         return self.__fields[index].field_with_ships()
 
     def field_without_ships(self, index):
+        """
+        Return field without ships as string
+        :param index: int
+        :return: str
+        """
         return self.__fields[index].field_without_ships()
 
     def shoot_at(self, index, tile):
+        """
+        Return True if player hit the ship, False otherwise,
+        string is he won.
+        :param index: int
+        :param tile: tuple
+        :return: bool/str
+        """
         hit = self.__fields[index - 1].shoot_at(tile)
         if not hit:
             return False
@@ -24,45 +56,89 @@ class Game:
         return True
 
     def check_win(self, index):
+        """Return True if player won"""
         if self.__fields[index - 1].field_with_ships().count('S') == 0:
             return True
 
 
 class Player:
+    """Class for layer representation"""
     def __init__(self, name='Bob'):
+        """
+        Create new Player
+        >>> player = Player('Marusia')
+        >>> player._Player__name
+        'Marusia'
+        """
         self.__name = name
 
     def read_position(self, tile):
+        """
+        Return indexes for list
+        :param tile: coordinates on field (tuple)
+        :return: tuple
+        """
         if type(tile[0]) == str:
             return (ord(tile[0]) - 65, tile[1] - 1)
         return tile
 
 
 class Ship:
+    """Class for Ship representation"""
     def __init__(self, bow, length, hit=[], horizontal=True):
+        """
+        Create new Ship
+        :param bow: tuple
+        :param length: tuple
+        :param hit: list
+        :param horizontal: bool
+
+        >>> ship = Ship((2,1), (4, 1))
+        >>> ship._Ship__length
+        (4, 1)
+        >>> ship.bow
+        (2, 1)
+        >>> ship.horizontal
+        True
+        """
         self.bow = bow
         self.__length = length
         self.__hit = hit[:] if hit else [False for _ in range(max(length))]
         self.horizontal = horizontal
 
     def shoot_at(self, tile):
+        """
+        Change Ship.__hit due to to the place where a ship was hit
+        :param tile: tuple
+        :return: None
+        """
         if self.horizontal:
             self.__hit[tile[1] - self.bow[1]] = True
         else:
             self.__hit[tile[0] - self.bow[0]] = True
 
     def __repr__(self):
+        """
+        Return Ship as string
+
+        >>> ship = Ship((2,1), (4, 1))
+        >>> ship
+        Ship((2, 1), (4, 1), [False, False, False, False], True)
+        """
         return '{}({}, {}, {}, {})'.format(type(self).__name__,
                                            self.bow, self.__length,
                                            self.__hit, self.horizontal)
 
 
 class Field:
+    """Class for field representation"""
     def __init__(self, ships=[]):
+        """Create new Field"""
         self.__ships = ships[:] if len(ships) > 0 else self.generate_board()
         self.show_ships = self.show_ship()
 
     def generate_board(self):
+        """Return randomly generated field"""
         field = generate_field()
         board = []
         ships = find_all_ships(field)
@@ -90,6 +166,10 @@ class Field:
         return board
 
     def show_ship(self):
+        """
+        Return field with all ships as a string
+        :return: str
+        """
         field = self.__ships[:]
         ships = []
         board = []
@@ -118,6 +198,7 @@ class Field:
         return board
 
     def shoot_at(self, tile):
+        """Return True is a ship was hit and false otherwise"""
         ships_all = self.show_ships
         ship = ships_all[tile[0]][tile[1]]
         board = self.__ships
@@ -137,6 +218,7 @@ class Field:
         return True
 
     def field_with_ships(self):
+        """Returns beautiful field with all ships"""
         field = self.show_ships
         result = '  '
         for i in range(1, 11):
@@ -157,6 +239,7 @@ class Field:
         return result
 
     def field_without_ships(self):
+        """Returns beautiful field without ships"""
         field = self.show_ships
         result = '  '
         for i in range(1, 11):
@@ -177,22 +260,30 @@ class Field:
 
 
 def main():
+    """Game cycle"""
     field1 = Field()
     field2 = Field()
 
     player1 = Player(input('Player1, type your name: '))
     player2 = Player(input('Player2, type your name: '))
 
+    game = Game([field1, field2], [player1, player2], 0)
+
+    if input('Do you want to play on field with all ships shown?'
+             '(yes, no) ') == 'yes':
+        field = game.field_with_ships
+    else:
+        field = game.field_without_ships
+
     print('═════════Game BATTLESHIP════════')
 
-    game = Game([field1, field2], [player1, player2], 0)
     while True:
         os.system('cls')
         move = ''
         print('\nField of the {}:\n'.format(
               game._Game__players[game._Game__current_player - 1].
               _Player__name))
-        print(game.field_without_ships(game._Game__current_player))
+        print(field(game._Game__current_player))
         while not move:
             move = input(game._Game__players[game._Game__current_player].
                          _Player__name + ', enter move (e.g. B2): ')
@@ -216,4 +307,6 @@ def main():
 
 
 if __name__ == '__main__':
+    import doctest
+    print(doctest.testmod())
     main()
